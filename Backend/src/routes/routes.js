fe43f15103b4623ee824c730ce05724aa96d906e
Tejas Gade <131139437+MernDev-Tejuu@users.noreply.github.com>
2023-08-31 22:03:04 +0530

    Update routes.js

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -17,7 +17,7 @@ app.use(cors(
          }
 ))
 
-app.get("https://hosting-pzug2qgle-merndev-tejuu.vercel.app/", function(req,res){
+app.get("https://hosting-4crfkit4v-merndev-tejuu.vercel.app/", function(req,res){
  res.send("Hello World Its working")        
 })
          
