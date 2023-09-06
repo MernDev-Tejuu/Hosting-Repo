c8f60c036f24bee6d16af52b637baeb206747feb
Tejas Gade <131139437+MernDev-Tejuu@users.noreply.github.com>
2023-09-06 20:49:40 +0530

    Update midW.js

--- Backend/src/middleware/midW.js
+++ Backend/src/middleware/midW.js
@@ -70,7 +70,7 @@ const user = require("../model/schema")
     const authenticate = async(req,res,next)=>{
       try{
         
-         window.alert("Flow went)
+         window.alert("Flow went")
           
          const tokenFetch = req.cookies.presenty
          if(!tokenFetch)window.alert('Something Went Wrong,Try to register again')
