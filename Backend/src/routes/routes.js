01df5d32817697c9ebfd67a0ca171157e15d65a9
FunctionUP-Teju <kapilsharma68@gmail.com>
2023-09-06 15:28:35 +0530

    mistake in origin url solved

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -12,7 +12,7 @@ app.use(express.json());
 //
 app.use(cors(
          {
-                  origin : 'https://tejascareer.com.netlify.app',
+                  origin : 'https://tejascareer.netlify.app',
                   methods : ["POST","GET"],
                   credentials :true
          }
