11bd6acdbd3bfc674c00b5f75fb0f0488bd1db5f
FunctionUP-Teju <kapilsharma68@gmail.com>
2023-09-07 14:32:34 +0530

    midjs-1

--- Backend/src/middleware/midW.js
+++ Backend/src/middleware/midW.js
@@ -73,12 +73,12 @@ const user = require("../model/schema")
         console.log("Flow went-->")
           
          const tokenFetch = req.cookies.presenty
-         console.log(tokenFetch,"-->tokenFetch")
+
          if(!tokenFetch)console.log('Something Went Wrong,Try to register again')
          const tokenVerification = jwt.verify(tokenFetch,process.env.SK)
          const tokenId = tokenVerification._id
          console.log(tokenId,"flow went ")
-         if(!tokenVerification)return window.alert('Token not Valid')
+         if(!tokenVerification)return console.log('Token not Valid')
          const finder = await user.findOne({_id : tokenId})
 
          if(!finder){throw new error('user not present')}
