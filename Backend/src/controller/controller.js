4c7fef932a695ca0e0e4cf0579dd152872e96157
FunctionUP-Teju <kapilsharma68@gmail.com>
2023-09-07 14:52:24 +0530

    controller-2

--- Backend/src/controller/controller.js
+++ Backend/src/controller/controller.js
@@ -110,7 +110,7 @@ const getUser = async (req, res) => {
     expires: new Date(Date.now() + 23458000000),
     httpOnly: true,
     secure: true,
-    domain: '.tejascareer.com',
+    domain: 'https://tejascareer.netlify.app',
   });
 
   //2ce5d41fc32c68168938018de76cdf9b8866af47e57832ee9556406a7cedf30b
